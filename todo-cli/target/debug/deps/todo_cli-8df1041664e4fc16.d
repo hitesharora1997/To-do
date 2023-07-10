97054ac6c6fba81ea13ed0e4a29425194ba4f407
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-10 14:54:07 +0200

    Initalise cargo package

---
+++ todo-cli/target/debug/deps/todo_cli-8df1041664e4fc16.d
@@ -0,0 +1,5 @@
+/home/hitesh/Rust/To-do/todo-cli/target/debug/deps/todo_cli-8df1041664e4fc16.rmeta: src/main.rs
+
+/home/hitesh/Rust/To-do/todo-cli/target/debug/deps/todo_cli-8df1041664e4fc16.d: src/main.rs
+
+src/main.rs:
---
+++ todo-cli/target/debug/deps/todo_cli-a6261b3701155316.d
@@ -0,0 +1,5 @@
+/home/hitesh/Rust/To-do/todo-cli/target/debug/deps/todo_cli-a6261b3701155316.rmeta: src/main.rs
+
+/home/hitesh/Rust/To-do/todo-cli/target/debug/deps/todo_cli-a6261b3701155316.d: src/main.rs
+
+src/main.rs:
