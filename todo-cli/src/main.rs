b7d31965990e9274c3778c06923ac45ab1aed10f
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-10 15:35:22 +0200

    Implement struct

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,6 +1,12 @@
+use std::collections::HashMap;
 fn main() {
-    let action = std::env::args().nth(1).expect("Please enter the action");
-    let item = std::env::args().nth(2).expect("Please enter the item");
+    let action = std::env::args().nth(1).expect("Please enter the action"); // Reading first arg
+    let item = std::env::args().nth(2).expect("Please enter the item"); // Reading second arg
+                                                                        // let j = env::args();
 
     println!("{:?}, {:?}", action, item);
+}
+
+struct Todo {
+    map: HashMap<String, bool>,
 }
