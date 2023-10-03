59863f03b70546c118f46bd31e458236946cb564
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-10-03 11:59:27 +0200

    new

--- todo-cli/src/error.rs
+++ todo-cli/src/error.rs
@@ -1,9 +1,10 @@
 //! Main Crate Error
 
-use std::error;
-
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Generic {0}")]
     Generic(String),
+
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
 }
